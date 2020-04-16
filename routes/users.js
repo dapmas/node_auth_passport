a2d291a0e0b0aabe944e3c58fed57da4174d44fd
dapmas <sampadmedda@gmail.com>
2020-04-16 13:20:00 +0530

    setup register page checks

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,37 @@ router.get('/register', (req, res) => {
   return res.render('register');
 });
 
+// Register handle
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields.' });
+  }
+
+  // Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not match.' });
+  }
+
+  // Check passwords length
+  if (password.length < 6) {
+    errors.push({ msg: 'Password should be atleast 6 chracters.' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    res.send('pass');
+  }
+});
+
 module.exports = router;
