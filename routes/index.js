40161521023d9d57de2f222c087e4d380f42540f
dapmas <sampadmedda@gmail.com>
2020-04-17 23:25:59 +0530

    ensure routes are protected

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../lib/auth'); // Use this to ensure that route is protected
 
+// Welcome page
 router.get('/', (req, res) => {
   return res.render('welcome');
+});
+
+// // Dashboard Page Unprotected
+// router.get('/dashboard', (req, res) => {
+//   return res.render('dashboard');
+// });
+
+// Dashboard Page Protected
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  return res.render('dashboard', {
+    userName: req.user.name,
+  });
 });
 
 module.exports = router;
